a3596d35e3792325b15efe28ede6dde3eb66913a
YahorKavaliou1 <kovalevegormail@gmail.com>
2026-08-28 16:32:30 +0200

    udpate configs based on the models info

--- passport_ocr/config.py
+++ passport_ocr/config.py
@@ -8,12 +8,39 @@ OCR_LANGUAGES = "rus+eng"
 
 REQUIRED_FIELDS = ["last_name", "first_name", "birth_date", "document_number"]
 
+DATE_FIELDS = {"birth_date", "issue_date", "expiry_date"}
+
+MRZ_PRIORITY_FIELDS = {
+    "last_name",
+    "first_name",
+    "birth_date",
+    "document_number",
+    "expiry_date",
+    "nationality",
+    "sex",
+    "personal_number",
+}
+
+VISUAL_ONLY_FIELDS = {
+    "middle_name",
+    "issue_date",
+    "issued_by",
+    "birth_place",
+    "citizenship",
+}
+
 FIELD_PATTERNS = {
     "last_name": r"Фамилия[:\s]+([А-ЯЁ\-]+)",
     "first_name": r"Имя[:\s]+([А-ЯЁ\-]+)",
     "middle_name": r"Отчество[:\s]+([А-ЯЁ\-]+)",
     "birth_date": r"Дата рождения[:\s]+(\d{2}\.\d{2}\.\d{4})",
     "document_number": r"(?:№|N)\s*([А-Я]{2}\d{7})",
+    "personal_number": r"(?:Идентификационный номер|Идентиф\. номер)[:\s]+([\dA-Z]+)",
     "issue_date": r"Дата выдачи[:\s]+(\d{2}\.\d{2}\.\d{4})",
+    "expiry_date": r"(?:Действителен до|Срок действия)[:\s]+(\d{2}\.\d{2}\.\d{4})",
     "issued_by": r"Выдан[:\s]+(.+)",
+    "nationality": r"(?:Код государства|Nationality)[:\s]+([A-Z]{3})",
+    "sex": r"Пол[:\s]+([МЖMF]|Муж\.?|Жен\.?|Male|Female)",
+    "birth_place": r"Место рождения[:\s]+(.+)",
+    "citizenship": r"Гражданство[:\s]+(.+)",
 }
